be24b622c7e7c2013a2c8a4bce5fe821dcbb133e
prinsergioprin <sergioprin24@gmail.com>
2025-01-13 17:01:31 +0100

    Handle button clicks and check for the right answer

--- main.js
+++ main.js
@@ -19,6 +19,7 @@ async function fetchQuestion() {
 }
 
 function showQuestion(questions) {
+  resetState();
   let correctAnswer = questions.correct_answer;
   let incorrectAnswer = questions.incorrect_answers;
   let answerList = incorrectAnswer;
@@ -27,13 +28,49 @@ function showQuestion(questions) {
     0,
     correctAnswer
   );
-  questionElement.innerHTML = `${questions.question}`;
-  answersElement.innerHTML = `${answerList
-    .map(
-      (answer, index) =>
-        `<button class="btn"> ${index + 1}. ${answer} </button>`
-    )
-    .join("")}`;
+  questionElement.innerHTML = questions.question;
+  answerList.forEach((answer) => {
+    const button = document.createElement("button");
+    button.innerText = answer;
+    button.classList.add("btn");
+    if (answer === correctAnswer) {
+      button.dataset.correct = "true";
+    }
+    button.addEventListener("click", selectAnswer);
+    answersElement.appendChild(button);
+  });
+}
+
+function resetState() {
+  clearStatusClass(document.body);
+  nextButton.classList.add("hide");
+  while (answersElement.firstChild) {
+    answersElement.removeChild(answersElement.firstChild);
+  }
+}
+
+function selectAnswer(e) {
+  const selectedButton = e.target;
+  const correct = selectedButton.dataset.correct === "true";
+  setStatusClass(document.body, correct);
+  Array.from(answersElement.children).forEach((button) => {
+    setStatusClass(button, button.dataset.correct === "true");
+  });
+  nextButton.classList.remove("hide");
+}
+
+function setStatusClass(element, correct) {
+  clearStatusClass(element);
+  if (correct) {
+    element.classList.add("correct");
+  } else {
+    element.classList.add("wrong");
+  }
+}
+
+function clearStatusClass(element) {
+  element.classList.remove("correct");
+  element.classList.remove("wrong");
 }
 
 fetchQuestion();
